469545eabb58fa4b0136799b7ee2d597ea7f9c08
John Samuel <johnsamuelwrites@gmail.com>
2020-03-21 18:33:22 +0100

    Add command line option

---
+++ main.py
@@ -0,0 +1,20 @@
+#
+# SPDX-FileCopyrightText: 2020 John Samuel <johnsamuelwrites@gmail.com>
+#
+# SPDX-License-Identifier: GPL-3.0-or-later
+#
+
+import argparse
+from shexstatements.shexfromcsv import CSV
+
+parser = argparse.ArgumentParser(prog='shexstatements')
+parser.add_argument('-o','--output', type=str, help='output file')
+parser.add_argument('csvfile', type=str, help='path of CSV file')
+
+args = parser.parse_args()
+shexstatement = CSV.generate_shex_from_csv(args.csvfile)
+if args.output:
+  with open(args.output, 'w') as shexfile:
+    shexfile.write(shexstatement)
+else:
+  print(shexstatement)
