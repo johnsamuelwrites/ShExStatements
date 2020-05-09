1be487cb004d259862751cd0b3f7437f8df66a2b
John Samuel <johnsamuelwrites@gmail.com>
2020-05-10 00:38:57 +0200

    Add support for online generation

--- app.py
+++ app.py
@@ -1,11 +1,26 @@
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template, url_for, request, redirect
+from shexstatements.shexfromcsv import CSV
 
 app = Flask(__name__, static_url_path='',
               template_folder='templates')
 
-@app.route('/')
-def index():
-  return render_template('shexstatements.html')
+@app.route('/', methods=['GET','POST'])
+#def index():
+#  return render_template('shexstatements.html')
+#
+#@app.route('/generateshex', methods=['POST'])
+def generateshex():
+  data = {}
+  if request.method == "POST":
+    shexstatements = request.form['shexstatements']
+    delim = request.form['delim']
+    shex = CSV.generate_shex_from_csv(shexstatements, delim=delim, filename=False)
+    data["input"] = shexstatements
+    data["output"] = shex
+    return render_template('shexstatements.html', data=data)
+  else:
+    return render_template('shexstatements.html', data=data)
+
 
 @app.route('/quickstart')
 def quickstart():
