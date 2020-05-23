ba01bf9f31324758275e651f181c2e3c227a705d
John Samuel <johnsamuelwrites@gmail.com>
2020-05-23 09:55:46 +0200

    Update setup file

--- setup.py
+++ setup.py
@@ -4,14 +4,17 @@
 # SPDX-License-Identifier: GPL-3.0-or-later
 #
 
-import setuptools
+import setuptools,runpy
+
+version_meta = runpy.run_path("./shexstatements/version.py")
+VERSION = version_meta["__version__"]
 
 with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
     name="shexstatements",
-    version="0.1",
+    version=VERSION,
     author="John Samuel",
     author_email="johnsamuelwrites@example.com",
     description="Tool to generate shape expressions from CSV files",
@@ -19,14 +22,16 @@ setuptools.setup(
     long_description_content_type="text/markdown",
     url="https://github.com/johnsamuelwrites/ShExStatements",
     packages=setuptools.find_packages(),
+    scripts=["shexstatements.sh"],
     classifiers=[
         "Programming Language :: Python :: 3",
-        "License :: OSI Approved :: GPL v 3.0 or later",
+        "License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)",
         "Operating System :: OS Independent",
     ],
     install_requires=[
         'ply>=3.11',
-        'PyShEx>=0.7.0',
+        'Flask>=1.1.2',
+        'PyShEx>=0.7.14',
     ],
     python_requires='>=3.6',
 )
